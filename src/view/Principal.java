644847a3de92e551286cbbb4d31e84318a5b84b9
Edilson <edilson.silva33@fatec.sp.gov.br>
2022-08-18 09:45:14 -0300

    Projeto3

---
+++ src/view/Principal.java
@@ -0,0 +1,19 @@
+package view;
+import javax.swing.JOptionPane;
+import controller.Controller;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int qtde;
+		double soma;
+
+		Controller somat = new Controller();
+		
+	    qtde = Integer.parseInt(JOptionPane.showInputDialog("Qtde = "));
+	    soma =  somat.Somatorio(qtde);
+	    
+		System.out.println("Somatório = "+soma);
+	}
+
+}
