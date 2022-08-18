7ba8b7313c6a0db3a70a9f59b6788ead331301f8
Edilson <edilson.silva33@fatec.sp.gov.br>
2022-08-18 15:55:27 -0300

    Projeto3

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package controller;
 
 public class Controller {
@@ -19,3 +20,26 @@ public class Controller {
 	 return soma;
 	}
 }
+=======
+package controller;
+
+public class Controller {
+
+	public Controller() {
+		super();
+	}
+	public double Somatorio(int qtde) {
+		double soma=0;
+		double variavel;
+// Ponto de parada definido como qtde = 1
+		if  (qtde==1) {
+			return 1;
+		}
+		else {
+		variavel = qtde;
+		soma = (1.0 / variavel) + Somatorio(qtde - 1);
+		}
+	 return soma;
+	}
+}
+>>>>>>> Stashed changes
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -13,7 +13,7 @@ public class Principal {
 	    qtde = Integer.parseInt(JOptionPane.showInputDialog("Qtde = "));
 	    soma =  somat.Somatorio(qtde);
 	    
-		System.out.println("Somatório = "+soma);
+		System.out.println("Somatorio = "+soma);
 	}
 
 }
